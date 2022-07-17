f452a2ae7c06356fc9bd166bdafe4e3c568771c7
caoyu-dev <choyumin01@gmail.com>
2022-07-17 12:48:27 +0900

    [Fix] P10986 확인용 텍스트 추가

--- src/Do_It/P10986.java
+++ src/Do_It/P10986.java
@@ -11,29 +11,29 @@ public class P10986 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-//        System.out.println("N --> " + N);
-//        System.out.println("M --> " + M);
+        System.out.println("N --> " + N);
+        System.out.println("M --> " + M);
 
         int sum = 0;
         int[] cnt = new int[M];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             sum += Integer.parseInt(st.nextToken()) % M;
-//            System.out.println("=======================");
-//            System.out.println(i + "번째 도는 중");
-//            System.out.println("sum --> " + sum);
-//            System.out.println("=======================");
+            System.out.println("=======================");
+            System.out.println(i + "번째 도는 중");
+            System.out.println("sum --> " + sum);
+            System.out.println("=======================");
             cnt[sum % M]++;
-//            System.out.println("cnt[" + sum % M + "]++ ==> " + cnt[sum % M]);
+            System.out.println("cnt[" + sum % M + "]++ ==> " + cnt[sum % M]);
         }
 
         long ans = cnt[0];
         for (int i = 0; i < M; i++) {
             ans += (long) cnt[i] * (cnt[i] - 1) / 2;
-//            System.out.println("=======================");
-//            System.out.println("cnt[" + i + "] --> " + cnt[i]);
-//            System.out.println("ans --> " + ans);
-//            System.out.println("=======================");
+            System.out.println("=======================");
+            System.out.println("cnt[" + i + "] --> " + cnt[i]);
+            System.out.println("ans --> " + ans);
+            System.out.println("=======================");
         }
         System.out.println(ans);
     }
